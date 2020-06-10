e9782a9e8a446db5b444594247ad8bb76f0b081d
teekaytech <taofeek.olalere@feghas.com>
2020-06-10 12:33:40 +0100

    Modify title (dynamic) with application_helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,6 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
