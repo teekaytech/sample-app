7c30b3d551c4aed2815f042cea9f2d9b56b9373b
teekaytech <taofeek.olalere@feghas.com>
2020-06-13 13:39:02 +0100

    Prepared the signup page

--- test/helpers/application_helper_test.rb
+++
@@ -1,8 +0,0 @@
- require 'test_helper'
-
-class ApplicationHelperTest < ActionView::TestCase
-  test "full title helper" do
-    assert_equal full_title,  "Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
-  end
-end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,7 +9,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    get contact_path
-    assert_select "title", full_title("Contact")
+    assert_select "a[href=?]", signup_path
   end
 end
